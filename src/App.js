33ff05c393db9e7dafd297f9f102ec98af7e383c
jahids <evanjahid321@gmail.com>
2022-09-05 22:30:20 -0700

    check

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import SimpleHome from './SimpleHome';
+
+function App() {
+  return (
+
+      <SimpleHome/>
+  
+  )
+}
+
+export default App;
---
+++ src/SimpleHome.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {DateRangePicker} from 'rsuite';
+import 'rsuite/dist/rsuite.min.css'; // or 'rsuite/dist/rsuite.min.css'
+
+const SimpleHome = () => {
+
+
+    return (
+
+        <div>
+                
+                <DateRangePicker 
+                format="MM/dd/yyyy"
+                defaultCalendarValue={
+                [new Date('2022-02-01 00:00:00'), new Date('2022-03-01 23:59:59')]
+                }/>
+        </div>
+
+
+    )
+};
+
+export default SimpleHome;
